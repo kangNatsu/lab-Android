187e179ce2736c3cefb67177e5345d0b465958a5
kangNatsu <37990020+kangNatsu@users.noreply.github.com>
2018-04-13 13:41:24 +0900

    Update MainActivity.java
    find Button

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -3,6 +3,7 @@ package edu.android.gitproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+impory android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
 
     }
 }
